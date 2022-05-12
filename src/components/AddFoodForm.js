3567d8f74a01f72f196d3ccb8f3a16ee92bf8ed4
Larry Kagan <larry@superiocity.com>
2022-05-12 12:12:47 -0400

    Add food

--- src/components/AddFoodForm.js
+++ src/components/AddFoodForm.js
@@ -1,21 +1,34 @@
 import { Divider, Input } from 'antd';
 
 function AddFoodForm({addFood}) {
+
+    const food = {
+        name: '',
+        image: '',
+        calories: '',
+        servings: ''
+    }
+
+    const addFoodHandler = (e) => {
+        e.preventDefault();
+        addFood(food);
+    }
+
   return (
-    <form onSubmit={addFood}>
+    <form onSubmit={addFoodHandler}>
       <Divider>Add Food Entry</Divider>
 
       <label>Name</label>
-      <Input value={undefined} name="name" type="text" onChange={() => {}} />
+      <Input value={undefined} name="name" type="text" onChange={e => food.name = e.target.value} />
 
       <label>Image</label>
-      <Input value={undefined} name="image" type="text" onChange={() => {}} />
+      <Input value={undefined} name="image" type="text" onChange={e => food.image = e.target.value} />
 
       <label>Calories</label>
-      <Input value={undefined} name="calories" type="number" onChange={() => {}} />
+      <Input value={undefined} name="calories" type="number" onChange={e => food.calories = e.target.value} />
 
       <label>Servings</label>
-      <Input value={undefined} name="servings" type="number" onChange={() => {}} />
+      <Input value={undefined} name="servings" type="number" onChange={e => food.servings = e.target.value} />
 
       <button type="submit">Create</button>
     </form>
