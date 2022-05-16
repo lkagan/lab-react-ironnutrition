7a341ab4a241d169ebb30525f556ffcfd26ed8f5
Kamila Kowalczyk <kamila.kowalczyk@icloud.com>
2022-05-15 22:04:58 -0400

    iteration 5 done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Search from './components/Search';
 import { useState } from 'react';
 import './App.css';
 import AddFoodForm from './components/AddFoodForm';
@@ -7,16 +8,25 @@ import foodsImport from './foods.json';
 
 function App() {
   const [foods, setFoods] = useState(foodsImport);
+  const [foodsCopy, setFoodsCopy] = useState(foods);
+
+  const searchHandler = (e) => {
+    setFoodsCopy(foods.filter(food => {
+      return food.name.toLowerCase().includes(e.target.value.toLowerCase());
+    }))
+  }
 
   const addFood = (food) => {
     setFoods([food, ...foods]);
   }
   return (
     <div className="App">
+     <h2>Search</h2>
+    <Search searchHandler={searchHandler} />
     <AddFoodForm addFood={addFood}/>
       <h1>Food List</h1>
       
-      {foods.map((food) => {
+      {foodsCopy.map((food) => {
         return (
           <FoodBox
         food={food} key={food.name}
